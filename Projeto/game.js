76b07e57e4991239b1ea509bd491cb86a7c3db31
Dr-Verdin <piscioneri24122004@gmail.com>
2024-12-28 14:40:36 -0300

    creating map

---
+++ Projeto/game.js
@@ -0,0 +1,69 @@
+const canvas = document.getElementById('canvas');
+const canvasContext = canvas.getContext("2d");
+const pacmanFrames = document.getElementById("pacman");
+const ghostFrames = document.getElementById("ghosts");
+
+let createRect = (x, y, width, height, color) => {
+    canvasContext.fillStyle = color;
+    canvasContext.fillRect(x, y, width, height);
+};
+
+let fps = 30;
+let oneBlockSize = 20;
+let wallColor = "#2121de";
+
+let map = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1],
+    [1, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 1],
+    [1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1],
+    [1, 1, 1, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 1, 1, 1],
+    [1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1],
+    [2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1],
+    [1, 1, 1, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 1, 1, 1],
+    [1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1],
+    [1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1],
+    [1, 1, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 1, 1],
+    [1, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 1],
+    [1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+];
+
+let gameLoop = () => {
+    update()
+    draw()
+};
+
+let update = () => {
+    // Update game state
+};
+
+let draw = () => {
+    // Draw
+    drawWalls()
+};
+
+let gameInterval = setInterval(gameLoop, 1000/fps);
+
+let drawWalls = () => {
+    for(let i = 0; i  < map.length; i++){
+        for(let j = 0; j < map[0].length; j++){
+            if(map[i][j] == 1){
+                createRect(
+                    j * oneBlockSize, 
+                    i * oneBlockSize, 
+                    oneBlockSize, 
+                    oneBlockSize, 
+                    wallColor
+                );
+            }
+        }
+    }
+};
