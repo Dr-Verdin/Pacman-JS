101394e9736e2da9dc18d99f13be7b91e00f2614
Dr-Verdin <piscioneri24122004@gmail.com>
2024-12-28 23:52:47 -0300

    getting keycodes of wasd and arrow keys

--- Projeto/game.js
+++ Projeto/game.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById('canvas');           // Obtem o elemento do canvas no HTML
 const canvasContext = canvas.getContext("2d");              // Obtem o contexto 2d do canvas, que permite desenhar nele
-const pacmanFrames = document.getElementById("pacman");     // Obtem o elemento de imagem com o id "pacman" para animar o Pac-Man
+const pacmanFrames = document.getElementById("animations");     // Obtem o elemento de imagem com o id "pacman" para animar o Pac-Man
 const ghostFrames = document.getElementById("ghosts");      // Obtem o elemento de imagem com o id "ghosts" para animar os fantasmas
 
 // Função para desenhar retângulos (usadas paras paredes e outros elementos)
@@ -9,13 +9,18 @@ let createRect = (x, y, width, height, color) => {
     canvasContext.fillRect(x, y, width, height);    // Desenha o retangulo
 };
 
-// COnfigurações do jogo:
+// Configurações do jogo:
 let fps = 30;                                               // Frames por segundo (taxa de atualização do jogo)
 let oneBlockSize = 20;                                      // Tamamho de cada "bloco" no mapa
 let wallColor = "#2121de";                                  // Cor das paredes (azul)
 let wallSpaceWidth = oneBlockSize / 1.3;                    // Largura das bordas internas das paredes
 let wallOffset = (oneBlockSize - wallSpaceWidth) / 2;       // Distancia entre as bordas internas
 let wallInnerColor = "black";                               // Cor das bordas internas das paredes (preto)
+
+const DIRECTION_RIGHT = 4;
+const DIRECTION_UP = 3;
+const DIRECTION_LEFT = 2;
+const DIRECTION_BOTTOM = 1;
 
 // Mapa do jogo: 1 representa as paredes e 2 representa espaços vazios
 let map = [
@@ -50,13 +55,14 @@ let gameLoop = () => {
 
 // Função para atualizar o estado do jogo
 let update = () => {
-    // Update game state
+    pacman.moveProcess()
 };
 
 // Função para desenhar tudo na tela
 let draw = () => {
     createRect(0, 0, canvas.width, canvas.height, "black"); // Limpa a tela, desenhando um retângulo preto para o fundo
-    drawWalls()                                             // Desenha as paredes do labirinto
+    drawWalls();                                            // Desenha as paredes do labirinto
+    pacman.draw();
 };
 
 // Intervalo que chama a função gameLoop a cada "1/fps" segundos
@@ -119,3 +125,32 @@ let drawWalls = () => {
         }
     }
 };
+
+let createNewPacman =() => {
+    pacman = new Pacman(
+        oneBlockSize,
+        oneBlockSize,
+        oneBlockSize,
+        oneBlockSize,
+        oneBlockSize / 5
+    )
+};
+
+createNewPacman();
+gameLoop();
+
+window.addEventListener("keydown", () => {
+    let k = event.keyCode;
+
+    setTimeout(() => {
+        if(k == 37 || k == 65){ // left
+            pacman.nextDirection = DIRECTION_LEFT;
+        } else if(k == 38 || k == 87){ // up
+            pacman.nextDirection = DIRECTION_UP;
+        } else if(k == 39 || k == 68){ // right
+            pacman.nextDirection = DIRECTION_RIGHT;
+        } else if(k == 40 || k == 83){ // bottom
+            pacman.nextDirection = DIRECTION_BOTTOM;
+        }
+    }, 1);
+})
